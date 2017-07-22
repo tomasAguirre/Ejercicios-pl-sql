3b27c282063d8bd35d6f397a21898a7167aa4ee9
Jonathan Aguirre <jonathan3aguirre@gmail.com>
2017-07-22 16:59:17 -0600

    Add files via upload

--- Ejemplo1-ComprobarNacionalidad.sql
+++ Ejemplo1-ComprobarNacionalidad.sql
@@ -3,21 +3,21 @@
 -- y compara la nacionalidad para ver si es argentino.
 set serverout on -- serverout on sirve para poder imprimir datos
 
-declare --declaracion de variables
+declare     --declaracion de variables
     region varchar(20);
-begin  --seleciono el primer elemento de la tabla countries
-    select country_name into region --con into guardamos dentro de la 
-    from COUNTRIES                  -- variable region el country name
-    where rownum = 1; --rownum nos permite elegir el primer elemento de la tabla
+begin      --seleciono el primer elemento de la tabla countries
+    select country_name into region     --con into guardamos dentro de la 
+    from COUNTRIES                      -- variable region el country name
+    where rownum = 1;     --rownum nos permite elegir el primer elemento de la tabla
     
-    if region like 'Argentina' then -- comparo si la region es de argentina
-        dbms_output.put_line('es argentino'); --esta instrucion sirve para imprimir en consola
+    if region like 'Argentina' then     -- comparo si la region es de argentina
+        dbms_output.put_line('es argentino');     --esta instrucion sirve para imprimir en consola
         else
         dbms_output.put_line('no es argentino');
-    end if; -- el if en pl/sql se cierra con punto y coma
+    end if;     -- el if en pl/sql se cierra con punto y coma
     
-exception --- capturo alguna exepcion que se llegue a presentar
-    when others then -- con others puedo capturar la exepcion sin importar cual sea
+exception     --- capturo alguna exepcion que se llegue a presentar
+    when others then     -- con others puedo capturar la exepcion sin importar cual sea
         dbms_output.put_line('Algo salio mal'); 
 end;
 / -- esta pleca invertida indica el final del bloque 
